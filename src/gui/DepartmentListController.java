f3d06dbb012782af5da83a0dfc43da4428470104
Kaic Furushima <kaic.fbs@gmail.com>
2024-06-13 18:33:59 -0300

    Initializing action as parameter for loadView

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -40,7 +40,7 @@ public class DepartmentListController implements Initializable {
         System.out.println("onBtNewAction");
     }
 
-    public void setService(DepartmentService service) {
+    public void setDepartmentService(DepartmentService service) {
         this.service = service;
     }
 
